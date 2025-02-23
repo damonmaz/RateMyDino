e84aada9901a87c7b91c7bb2ccb9253e8b7871d9
Aleks B <ajrberezowski@gmail.com>
2025-02-23 13:24:18 -0700

    base scraping

---
+++ web-scraping.py
@@ -0,0 +1,26 @@
+import re
+import requests
+import pandas as pd
+
+comment_regex = r'"comment":"([^"]+)"'
+class_regex = r'"class":"([^"]+)"'
+helpful_regex = r'"helpfulRating":([^,]+),'
+clarity_regex = r'"clarityRating":([^,]+),'
+difficulty_regex = r'"difficultyRating":([^,]+),'
+
+def get_professor_ratings(professor_id: int):
+    r = requests.get(f'https://www.ratemyprofessors.com/professor/{professor_id}')
+    content = str(r.content)
+
+    data = {
+        "class": re.findall(class_regex, content),
+        "comment": re.findall(comment_regex, content),
+        "helpful_rating": re.findall(helpful_regex, content),
+        "clarity_rating": re.findall(clarity_regex, content),
+        "difficulty_rating": re.findall(difficulty_regex, content)
+    }
+
+    return pd.DataFrame(data)
+
+if __name__ == "__main__":
+    get_professor_ratings(1469464)
