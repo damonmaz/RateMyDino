137af0e2cf396255c6f7915a2fa3293fed7a8af5
Aadi Chauhan <aadichauhan321@gmail.com>
2025-02-24 16:41:21 -0700

    get_professor_name function

--- webscraping.py
+++ webscraping.py
@@ -2,6 +2,12 @@ import re
 import requests
 import pandas as pd
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 comment_regex = r'"comment":"([^"]+)"'
 class_regex = r'"class":"([^"]+)"'
 helpful_regex = r'"helpfulRating":([^,]+),'
@@ -25,10 +31,23 @@ def get_professor_ratings(professor_id: int, class_id: str) -> pd.DataFrame:
     return df[df["class"] == class_id]
 
 def get_professor_name(professor_id: int) -> str:
-    r = requests.get(f'https://www.ratemyprofessors.com/professor/{professor_id}')
-    content = str(r.content)
+    url = f'https://www.ratemyprofessors.com/professor/{professor_id}'
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver.get(url)
+
+    try:
+        name_element = driver.find_element(By.CLASS_NAME, "NameTitle__NameWrapper-dowf0z-2")
+        return name_element.text
+    except:
+        return "Could not find professor's name. The website structure may have changed."
 
     
 
 if __name__ == "__main__":
     print(get_professor_ratings(1469464, "ENEL453"))
+
+    print(get_professor_name(1469464))
