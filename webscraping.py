45d2c76887094c2ac419986f91b98a854b849030
Aadi Chauhan <aadichauhan321@gmail.com>
2025-02-24 17:20:01 -0700

    get_professor_tags
    gets the top tags of the chosen prof

--- webscraping.py
+++ webscraping.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 comment_regex = r'"comment":"([^"]+)"'
@@ -45,9 +47,39 @@ def get_professor_name(professor_id: int) -> str:
     except:
         return "Could not find professor's name. The website structure may have changed."
 
-    
+
+def get_professor_tags(professor_id: int) -> list:
+
+    url = f'https://www.ratemyprofessors.com/professor/{professor_id}'
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver.get(url)
+    try:
+
+        tags_container = WebDriverWait(driver, 10).until( # we need to get the tags from only this container, otherwise it returns tags from some random place (i.e. comments and stuff, we only want top tags)
+            EC.presence_of_element_located((By.CLASS_NAME, "TeacherTags__TagsContainer-sc-16vmh1y-0"))
+        )
+        
+        tags = tags_container.find_elements(By.CLASS_NAME, "Tag-bs9vf4-0")
+
+        tag_list = [tag.text for tag in tags] # for some reason this returns duplicates of the same tag multiple times, so we will convert this into a set and reconvert it into a list at the end
+        tag_list = set(tag_list)
+        tag_list = list(tag_list)
+
+        return tag_list
+
+    except:
+        return []
 
 if __name__ == "__main__":
-    print(get_professor_ratings(1469464, "ENEL453"))
 
-    print(get_professor_name(1469464))
+    OnenTag = 1469464
+    # print(get_professor_ratings(OnenTag, "ENEL453"))
+
+    # print(get_professor_name(OnenTag))
+
+    tags = get_professor_tags(OnenTag)
+    print(tags)
