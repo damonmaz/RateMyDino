8a924c3b71e36d0ca5cccc813a6a0b89bc3694e4
Aadi Chauhan <aadichauhan321@gmail.com>
2025-03-10 13:50:24 -0600

    Done getPID

--- src/scraper/GetPID.py
+++ src/scraper/GetPID.py
@@ -9,20 +9,24 @@ def extract_professors(filename):
     with open(filename, "r", encoding="utf-8") as file:
         soup = BeautifulSoup(file, "html.parser")
     
-    professors = []
+    professors = {}
     
-    counter = 0
-
-    cardcount  = []
-
     # Find all professor divs
     for card in soup.find_all("a", class_="TeacherCard__StyledTeacherCard-syjs0d-0 dLJIlx"):
         
         for info_rating_wrapper in card.find_all("div", class_="TeacherCard__InfoRatingWrapper-syjs0d-3 kcbPEB"):
+
+            # Get the URL value
+            pID = str(card['href']).replace("https://www.ratemyprofessors.com/professor/", "")
+
+
+            # Get the names
             for card_info in info_rating_wrapper.find_all("div", class_="TeacherCard__CardInfo-syjs0d-1 fkdYMc"):
                 for name_info in card_info.find_all("div", class_="CardName__StyledCardName-sc-1gyrgim-0 cJdVEK"):
-                    print(name_info.text)
+                    name = name_info.text
 
+            professors[name] = pID
+            
     return professors
 
 # Example usage
@@ -31,11 +35,4 @@ professors = extract_professors(filename)
 
 # Print results
 
-print(len(cardCount))
-
-
-if professors != []:
-    for name, url in professors:
-        print(f"{name}: {url}")
-else:
-    print("Shit aint found dawg")
+print(professors["Maryam Hachem"])
