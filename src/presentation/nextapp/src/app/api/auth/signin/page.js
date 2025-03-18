920013cc3cdc6e71c1405fa15770f1e09a72a42c
JaisumerS <sumer.sandhu@hotmail.com>
2025-03-17 21:54:42 -0600

    Installed react icons and fixed signup page

--- src/presentation/nextapp/src/app/api/auth/signin/page.js
+++ src/presentation/nextapp/src/app/api/auth/signin/page.js
@@ -2,6 +2,7 @@
 
 import { getProviders, signIn } from "next-auth/react";
 import { useEffect, useState } from "react";
+import { FaGoogle } from "react-icons/fa";
 
 export default function SignIn() {
   const [providers, setProviders] = useState(null);
@@ -15,18 +16,22 @@ export default function SignIn() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4 text-black">Sign in to Your Account</h1>
-      {providers &&
-        Object.values(providers).map((provider) => (
-          <button
-            key={provider.id}
-            onClick={() => signIn(provider.id)}
-            className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
-          >
-            Sign in with {provider.name}
-          </button>
-        ))}
+    <div className="flex items-center justify-center min-h-screen bg-navy">
+      <div className="bg-white border border-black p-8 rounded-lg shadow-lg w-80 sm:w-96 text-center">
+        <h1 className="text-xl font-bold mb-6 text-black">Sign in to Your Account</h1>
+        
+        {providers &&
+          Object.values(providers).map((provider) => (
+            <button
+              key={provider.id}
+              onClick={() => signIn(provider.id)}
+              className="flex items-center justify-center gap-2 w-full px-6 py-2 bg-red-500 text-white rounded-lg hover:bg-red-700 transition"
+            >
+              <FaGoogle className="text-xl" />
+              Sign in with {provider.name}
+            </button>
+          ))}
+      </div>
     </div>
   );
 }
